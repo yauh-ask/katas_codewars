3e4b90df5680106f55ac5ee3a0b48e6431f03ca2
yauh-ask <66479302+yauh-ask@users.noreply.github.com>
2020-11-23 13:48:31 +0100

    Update ispy.js

--- ispy.js
+++ ispy.js
@@ -14,3 +14,24 @@ function spyOn (func) {
   spy.returned = function (val) { return returnVals.indexOf(val) > -1; };
   return spy;
 };
+
+// OR
+
+function spyOn(func) {
+  let calls = 0
+  let all = []
+  let val
+  
+  const spy = function(...args) {
+    calls++
+    all.push(...args)
+    val = func.apply(this, args)
+    return val
+  }
+  
+  spy.callCount = () => calls
+  spy.wasCalledWith = (x) => all.some((a) => x === a)
+  spy.returned = (x) => x === val
+  
+  return spy
+}
