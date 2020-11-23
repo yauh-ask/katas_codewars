966232b0832efd24868d79ba2d1489d56dc20668
yauh-ask <66479302+yauh-ask@users.noreply.github.com>
2020-11-23 13:52:53 +0100

    Create calculating_with_functions.js

---
+++ calculating_with_functions.js
@@ -0,0 +1,9 @@
+['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+.forEach(function (name, n) {
+  this[name] = function (f) { return f ? f(n) : n }
+});
+
+function plus(n)      { return function (a) { return a + n } }
+function minus(n)     { return function (a) { return a - n } }
+function times(n)     { return function (a) { return a * n } }
+function dividedBy(n) { return function (a) { return a / n } }
